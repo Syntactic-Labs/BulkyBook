36fb36e4879d4988a9ddaafe3e6710ceed3a394b
Michael W <walkerrepair255@yahoo.com>
2021-12-21 16:43:08 -0500

    Add project files.

---
+++ BulkyBookWeb/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder builder) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BulkyBookWeb/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    [Index("Username", IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; } = 0;
+        [StringLength(30), Required]
+        public string Username { get; set; } = string.Empty;
+        [StringLength(30), Required]
+        public string Password { get; set; } = string.Empty;
+        [StringLength(30), Required]
+        public string Firstname { get; set; } = string.Empty;
+        [StringLength(30), Required]
+        public string Lastname { get; set; } = string.Empty;
+        [StringLength(12)]
+        public string? Phone { get; set; } = null;
+        [StringLength(255)]
+        public string? Email { get; set; } = null;
+        [Required]
+        public bool IsReviewer { get; set; } = false;
+        [Required]
+        public bool IsAdmin { get; set; } = false;
+    }
+}
