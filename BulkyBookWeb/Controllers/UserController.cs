f8baa743809da3158133e9add838bedb5a0a7a8e
Michael W <walkerrepair255@yahoo.com>
2021-12-21 21:43:45 -0500

    Add method to controller to get all Users to a table using MVC

---
+++ BulkyBookWeb/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _db;
+        public UserController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<User> objUserList = _db.Users;
+            return View(objUserList);
+        }
+    }
+}
