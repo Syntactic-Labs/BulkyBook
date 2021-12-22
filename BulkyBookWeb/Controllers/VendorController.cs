f38f54ed9c444398d7bdf5f8e5cd5f9777bf76e5
Michael W <walkerrepair255@yahoo.com>
2021-12-22 16:24:59 -0500

    add vendor view. needs tweeking

---
+++ BulkyBookWeb/Controllers/VendorController.cs
@@ -0,0 +1,20 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class VendorController : Controller
+    {
+        private readonly AppDbContext _db;
+        public VendorController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Vendor> objVendorList = _db.Vendors;
+            return View(objVendorList);
+        }
+    }
+}
